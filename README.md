77ccbd95fe54a7db6fde898cac4eb1f03d879030
Dave <pzhang65@uwo.ca>
2021-02-08 10:59:01 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple RESTful blog API
+
+Learning how to create a simple RESTful blog in Python with Flask.
+
+Things I'll be exploring:
+* pipenv + setting environment variables*
+* psycopg2
+* marshmallow
+
+Stack includes:
+* Python
+* PostgreSQL
+* pyjwt (user authentication)
+* and more to come after
+
+Things to learn after*:
+* Docker containers to handle project dependencies and environment variables
+* React.js to complete blog site.
---
+++ src/config.py
@@ -0,0 +1,27 @@
+# /src/config.py
+
+import os
+
+
+class Development(object):
+    """
+    Development environment configuration
+    """
+    DEBUG = True
+    TESTING = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+class Production(object):
+    """
+    Production environment configurations
+    """
+    DEBUG = False
+    TESTING = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+app_config = {
+    'development': Development,
+    'production': Production,
+}
