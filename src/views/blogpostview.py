78951b1740411a7a94293c71f320bc30135954ff
Dave <>
2021-02-19 09:46:46 -0500

    Added more endpoints to blogpostview

--- src/views/blogpostview.py
+++ src/views/blogpostview.py
@@ -2,6 +2,7 @@
 from flask import request, json, Response, Blueprint, jsonify, g
 from marshmallow import ValidationError
 from ..models.blogpostmodel import BlogpostModel, BlogpostSchema
+from ..models.usermodel import UserModel
 from ..auth import Auth
 
 posts_api = Blueprint('posts', __name__)
@@ -26,13 +27,32 @@ def create():
     return custom_response(data,201)
 
 @posts_api.route('/', methods = ['GET'])
-@Auth.auth_required
 def get_all():
     posts = BlogpostModel.get_all_blogposts()
     data = posts_schema.dump(posts, many=True)
     return custom_response(data, 200)
 
-    
+@post_api.route('/<int:blogpost_id>', methods=['GET'])
+def get_one(blogpost_id):
+    posts = BlogpostModel.get_one_bologpost(blogpost_id)
+    if not posts:
+        return custom_response({'error':'post not found'}, 404)
+    data = posts_schema.dump(post)
+    return custom_response(data, 200)
+
+@post_api.route('/<int:owner_id>', methods=['GET'])
+def get_all_owners(owner_id):
+    user = UserModel.get_one_user(owner_id)
+    if not users:
+        return custom_response({'error': 'user does not exist!'}, 400)
+
+    posts = BlogpostModel.get_user_blogposts(owner_id)
+    if not posts:
+        return custom_response({'error': 'User does not have any posts'})
+
+    data = posts_schema.dump(post, many=True)
+    return custom_response(data, 200)
+
 
 
 def custom_response(res, status_code):
